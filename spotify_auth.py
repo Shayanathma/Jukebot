92013339236453a5bbb7d34a8dbe7aa357bbaaa5
Shayan Athma <shayanathma2305@gmail.com>
2025-04-30 14:09:48 +0530

    UI changes

--- spotify_auth.py
+++ spotify_auth.py
@@ -1,21 +1,18 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 import os
+from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 
 def get_spotify_token():
-   
-    # Setup Spotify Client Credentials
-    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET
+    )
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    
-    # Get the access token
     token_info = sp.auth_manager.get_access_token()
     return token_info['access_token']
-
